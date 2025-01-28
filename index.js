ce3130c06b1b4d290a335f9ad545ea608498dae1
Sabbir <sabbirmridha880@gmail.com>
2025-01-28 15:23:45 +0600

    add

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const port = process.env.PORT || 3000;
 const db = knex({
   client: 'mysql2',
   connection: {
+    connectionLimit: 10,
     host: '127.0.0.1',
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
